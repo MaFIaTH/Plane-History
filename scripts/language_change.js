7c233c55c34ba263b84e2fbcf8f7d479aba8fc59
MaFIaTH <mafeeyawat@gmail.com>
2024-04-25 16:04:56 +0700

    Only show the latest plane

--- scripts/language_change.js
+++ scripts/language_change.js
@@ -9,18 +9,20 @@ export const Diagnostics = require('Diagnostics');
   var wrightFoundBool = false;
   var dc3FoundBool = false;
   var boeingFoundBool = false;
+  var currentFoundArray = new Array(0);
 
   function sumItUp ()
   {
-    if (wrightFoundBool)
+    var lastIndex = currentFoundArray[currentFoundArray.length - 1];
+    if (wrightFoundBool && lastIndex == 0)
     {
-      planeIndex = 0;
+      planeIndex = 0;   
     }
-    else if (dc3FoundBool)
+    else if (dc3FoundBool && lastIndex == 1) 
     {
       planeIndex = 2;
     }
-    else if (boeingFoundBool)
+    else if (boeingFoundBool && lastIndex == 2)
     {
       planeIndex = 4;
     }
@@ -45,6 +47,22 @@ export const Diagnostics = require('Diagnostics');
     Patches.inputs.setBoolean('planeFound', false);
   }
 
+  function manageArray(found, index)
+  {
+    if (found == true && currentFoundArray.indexOf(index) == -1)
+    {
+      currentFoundArray.push(index);
+      resetAudio();
+    } 
+    else if (found == false) 
+    {
+      var removeIndex = currentFoundArray.indexOf(index);
+      if (index == currentFoundArray[currentFoundArray.length - 1]) resetAudio();
+      if (removeIndex > -1)
+        currentFoundArray.splice(removeIndex, 1);
+    }
+  }
+
   const languageIndex = await Patches.outputs.getScalar('languageIndex').then(event => 
   {
     event.monitor().subscribe
@@ -67,9 +85,9 @@ export const Diagnostics = require('Diagnostics');
         {
           wrightFoundBool = values.newValue;
           Diagnostics.log('Wright Found: ' + values.newValue);
+          manageArray(values.newValue, 0);
           isPlaneFound();
           sumItUp();
-          resetAudio();
         }
       )
     });
@@ -81,9 +99,9 @@ export const Diagnostics = require('Diagnostics');
         {
           dc3FoundBool = values.newValue;
           Diagnostics.log('DC3 Found: ' + values.newValue);
+          manageArray(values.newValue, 1);        
           isPlaneFound();
           sumItUp();
-          resetAudio();
         }
       )
     });
@@ -95,9 +113,9 @@ export const Diagnostics = require('Diagnostics');
           {
             boeingFoundBool = values.newValue;
             Diagnostics.log('Boeing Found: ' + values.newValue);
+            manageArray(values.newValue, 2);
             isPlaneFound();
             sumItUp();
-            resetAudio();
           }
         )
       });
